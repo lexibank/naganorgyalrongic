4a4a6260840c5c564231dc9092c85a3a8d3f77ef
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-03-12 14:00:43 +0100

    First pass compiling

---
+++ README.md
@@ -0,0 +1,27 @@
+# naganorgyalrongic
+
+Cite the source dataset as
+
+> Nagano, Yasuhiko and Prins, Marielle (eds.). 2013. rGyalrongic Languages Database. Minpaku.
+
+This dataset is licensed under a https://creativecommons.org/licenses/by-nc/4.0/ license
+
+## Statistics
+
+
+
+![Glottolog: 0%](https://img.shields.io/badge/Glottolog-0%25-red.svg "Glottolog: 0%")
+![Concepticon: 0%](https://img.shields.io/badge/Concepticon-0%25-red.svg "Concepticon: 0%")
+![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
+![BIPA: 17%](https://img.shields.io/badge/BIPA-17%25-red.svg "BIPA: 17%")
+![CLTS SoundClass: 18%](https://img.shields.io/badge/CLTS%20SoundClass-18%25-red.svg "CLTS SoundClass: 18%")
+
+- **Varieties:** 10
+- **Concepts:** 1,256
+- **Lexemes:** 10,089
+- **Synonymy:** 1.00
+- **Cognacy:** 0 cognates in 0 cognate sets
+- **Invalid lexemes:** 0
+- **Tokens:** 104,061
+- **Segments:** 595 (492 BIPA errors, 490 CTLS sound class errors, 103 CLTS modified)
+- **Inventory size (avg):** 109.10
--- lexibank_naganorgyalrongic.py
+++ lexibank_naganorgyalrongic.py
@@ -14,6 +14,16 @@ from tqdm import tqdm
 import csv
 import lingpy
 
+# Define minimum coverage in order to include a doculect
+MIN_COVERAGE = 500
+
+# Define glossing languages not be included
+SKIP_LANGS = [
+    'Tibetan (Script)',
+    'Chinese (Hanzi)',
+    'Japanese',
+]
+
 class Dataset(NonSplittingDataset):
     id = 'naganorgyalrongic'
     dir = Path(__file__).parent
@@ -25,6 +35,71 @@ class Dataset(NonSplittingDataset):
         pass
 
     def cmd_install(self, **kw):
-        print("should install")
-        pass
+        # Read raw data
+        raw_data = self.dir.joinpath('raw', 'YN-RGLD.csv').as_posix()
+        wl = lingpy.Wordlist(raw_data)
 
+        # Get the list of doculects with the minimum coverage
+        doculects = [
+            key for key, value in wl.coverage().items()
+            if value >= MIN_COVERAGE and key not in SKIP_LANGS]
+
+        # Collect rows for languoids with the minimum requested coverage
+        raw_entries = []
+        with open(raw_data) as handler:
+            reader = csv.DictReader(handler, delimiter='\t')
+            for row in reader:
+                # Extract the fields we are interested in
+                value = row['reflex']
+                gloss = row['gloss']
+                if row['gfn']:
+                    gloss = "%s (%s)" % (gloss, row['gfn'])
+                language = row['language']
+                srcid = row['srcid']
+
+                # Skip if no minimum coverage
+                if language not in doculects:
+                    continue
+
+                # Fix error in STEDT (bad parsing)
+                if row['rn'] == '621453':
+                    srcid = '1680'
+
+                # add to raw entries
+                raw_entries.append({
+                    'value' : value,
+                    'gloss' : gloss,
+                    'language' : language,
+                    'srcid' : srcid,
+                })
+
+        # add information to the dataset
+        with self.cldf as ds:
+            # add languages to the dataset
+            lang_map = {}
+            for language in self.languages:
+                ds.add_language(
+                    ID=language['SHORT_NAME'],
+                    Name=language['NAME'],
+                )
+                lang_map[language['NAME']] = language['SHORT_NAME']
+
+            # add concepts to the dataset
+            for concept in self.concepts:
+                ds.add_concept(
+                    ID=concept['SRCID'],
+                    Name=concept['GLOSS'],
+                )
+
+            # add lexemes
+            for idx, entry in tqdm(enumerate(raw_entries), desc='make-cldf'):
+                # !!! only for developing
+                segments = [c for c in entry['value']]
+                for row in ds.add_lexemes(
+                    Language_ID=lang_map[entry['language']],
+                    Parameter_ID=entry['srcid'],
+                    Form=entry['value'],
+                    Value=entry['value'],
+                    Segments=segments,
+                    Source=['Nagano2013']):
+                    pass
