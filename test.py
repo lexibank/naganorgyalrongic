89849eeafb2858ace1725c664c2ce39e43fcabe7
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-12-02 13:12:17 +0100

    Fix test

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
 
-
 def test_forms(cldf_dataset):
     assert len(list(cldf_dataset["FormTable"])) == 10085
-    assert any(f["Form"] == "ˊtə mȵa ˉlə˞ wɔ" for f in cldf_dataset["FormTable"])
+    assert any(f["Form"] == "ˊtɐ_ku" for f in cldf_dataset["FormTable"])
 
 
 def test_parameters(cldf_dataset):
